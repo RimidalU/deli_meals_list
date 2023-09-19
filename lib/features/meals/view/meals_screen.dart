07681faf11399d03fd4e71644a922983ef8f23bc
RimidalU <stankevichuuladimiru@gmail.com>
2023-09-20 01:02:50 +0300

    feat: implement base MealItem widget with image

--- lib/features/meals/view/meals_screen.dart
+++ lib/features/meals/view/meals_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../data/data.dart';
+import '../widgets/widgets.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
@@ -34,7 +35,13 @@ class MealsScreen extends StatelessWidget {
         padding: const EdgeInsets.all(15),
         child: ListView.builder(
           itemBuilder: (context, index) {
-            return Text(categoryMeals[index].title);
+            return MealItem(
+              affordability: categoryMeals[index].affordability,
+              complexity: categoryMeals[index].complexity,
+              duration: categoryMeals[index].duration,
+              imageUrl: categoryMeals[index].imageUrl,
+              title: categoryMeals[index].title,
+            );
           },
           itemCount: categoryMeals.length,
         ),
---
+++ lib/features/meals/widgets/meal_item.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+import '../models/meal_model.dart';
+
+class MealItem extends StatelessWidget {
+  const MealItem({
+    super.key,
+    required this.title,
+    required this.imageUrl,
+    required this.duration,
+    required this.complexity,
+    required this.affordability,
+  });
+
+  final String title;
+  final String imageUrl;
+  final int duration;
+  final Complexity complexity;
+  final Affordability affordability;
+
+  void handleSelectMeal(context) {}
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => handleSelectMeal(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        elevation: 8,
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                  child: Image.network(
+                    imageUrl,
+                    height: 250,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 0,
+                  child: Container(
+                    width: 250,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(15),
+                          bottomLeft: Radius.circular(15)),
+                      color: Colors.black54,
+                    ),
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                    child: Text(
+                      title,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
