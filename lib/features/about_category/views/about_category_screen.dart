c4b243b673fc9a20709e2fa2b97a7924cee59c24
RimidalU <stankevichuuladimiru@gmail.com>
2023-09-21 18:16:34 +0300

    feat: implement base AboutCategoryScreen widget

---
+++ lib/features/about_category/views/about_category_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AboutCategoryScreen extends StatelessWidget {
+  const AboutCategoryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('About Category Screen'));
+  }
+}
---
+++ lib/features/about_category/views/views.dart
@@ -0,0 +1,1 @@
+export './about_category_screen.dart';
