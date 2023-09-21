42a713c2320dcaa88a729eb2777e72b3d9be6a80
RimidalU <stankevichuuladimiru@gmail.com>
2023-09-21 19:28:19 +0300

    feat: implement buildListTitle

--- lib/features/main_drawer/view/main_drawer_screen.dart
+++ lib/features/main_drawer/view/main_drawer_screen.dart
@@ -3,11 +3,57 @@ import 'package:flutter/material.dart';
 class MainDrawerScreen extends StatelessWidget {
   const MainDrawerScreen({super.key});
 
+  Widget buildListTitle({required title, required IconData icon}) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 26,
+      ),
+      title: Text(
+        title,
+        style: const TextStyle(
+            fontFamily: 'RobotoCondensed',
+            fontSize: 24,
+            fontWeight: FontWeight.bold),
+      ),
+      onTap: () {},
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Drawer(
+    return Drawer(
       child: Center(
-        child: Text('MainDrawerScreen'),
+        child: Column(
+          children: [
+            Container(
+              height: 120,
+              width: double.infinity,
+              padding: const EdgeInsets.all(20),
+              alignment: Alignment.centerLeft,
+              color: Theme.of(context).secondaryHeaderColor,
+              child: Text(
+                'Cooking Up!',
+                style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 30,
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            buildListTitle(
+              title: 'Meals',
+              icon: Icons.restaurant,
+            ),
+            buildListTitle(
+              title: 'Filters',
+              icon: Icons.settings,
+            ),
+          ],
+        ),
       ),
     );
   }
