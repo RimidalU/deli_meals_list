305dd9397738f7560b7db9ea71f7399a589f2075
RimidalU <stankevichuuladimiru@gmail.com>
2023-09-22 21:30:36 +0300

    feat: implement Filter class

---
+++ lib/features/filters/models/filter_model.dart
@@ -0,0 +1,13 @@
+class Filter {
+  final bool glutenFree;
+  final bool vegetarian;
+  final bool vegan;
+  final bool lactoseFree;
+
+  const Filter({
+    required this.glutenFree,
+    required this.vegetarian,
+    required this.vegan,
+    required this.lactoseFree,
+  });
+}
