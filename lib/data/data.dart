873fdc95dbd3847ba578685ba4db4f2011b2aa5c
RimidalU <stankevichuuladimiru@gmail.com>
2023-09-23 17:39:02 +0300

    feat: refactor categories data

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,1 +1,2 @@
+export './dummy_categories.dart';
 export './dummy_meals.dart';
---
+++ lib/data/dummy_categories.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+import '/features/categories/models/models.dart';
+
+const dummyCategories = [
+  Category(
+      id: 'c1',
+      title: 'Italian',
+      color: Colors.purple,
+      description:
+          'The Mediterranean diet forms the basis of Italian cuisine, rich in pasta, fish, fruits and vegetables. Cheese, cold cuts and wine are central to Italian cuisine, and along with pizza and coffee (especially espresso) form part of Italian gastronomic culture.'),
+  Category(
+    id: 'c2',
+    title: 'Quick & Easy',
+    color: Colors.red,
+    description:
+        'Explore hundreds of top-rated quick and easy recipes for breakfast, lunch, and dinner.',
+  ),
+  Category(
+    id: 'c3',
+    title: 'Hamburgers',
+    color: Colors.orange,
+    description:
+        'A hamburger, or simply burger, is a sandwich consisting of fillings—usually a patty of ground meat, typically beef—placed inside a sliced bun or bread roll.',
+  ),
+  Category(
+    id: 'c4',
+    title: 'German',
+    color: Colors.amber,
+    description:
+        'Whilst there are regional variations in food culture, most German recipes focus heavily on bread, potatoes, and meat, especially pork, as well as plenty of greens such as types of cabbage and kale. Cake, coffee, and beer are all highly popular elements of German cuisine too - which will be good news to most! ',
+  ),
+  Category(
+    id: 'c5',
+    title: 'Light & Lovely',
+    color: Colors.blue,
+    description:
+        'Top 15 Light Food Recipes That Are Easy To Digest Light foods can be loosely described as foods that your body finds easy to digest and obtain nutrition from. A variety of fresh fruits and vegetables, some legumes, eggs, certain kinds of seafood, and chicken are common examples of light foods. These are also typically low in fats and spices.',
+  ),
+  Category(
+    id: 'c6',
+    title: 'Exotic',
+    color: Colors.green,
+    description:
+        'Exotic foods are generally foods that are not native to your country. Exotic foods are not foods that you find in most dishes. Spices are a great example of exotic foods. These can add fragrance and flavor. Rose water or flowers are also exotic.',
+  ),
+  Category(
+    id: 'c7',
+    title: 'Breakfast',
+    color: Colors.lightBlue,
+    description:
+        'The theory is that breakfast plays an important role in regulating appetite, hormone levels and the number of calories we burn throughout the day. Breakfast eaters tend to perform better in school. Breakfast eaters have better test scores, improved memory and focus, and are less likely to be absent or tardy.',
+  ),
+  Category(
+    id: 'c8',
+    title: 'Asian',
+    color: Colors.lightGreen,
+    description:
+        'Ingredients common to many cultures in the East and Southeast regions of the continent include rice, ginger, garlic, sesame seeds, chilies, dried onions, soy, and tofu. Stir frying, steaming, and deep frying are common cooking methods.',
+  ),
+  Category(
+    id: 'c9',
+    title: 'French',
+    color: Colors.pink,
+    description:
+        'French dishes often use simple ingredients transformed by artful techniques. Fresh, naturally produced ingredients can always be found in French cuisine. Wine, cheese, olive oil, and seasonal vegetables are just a few staples.',
+  ),
+  Category(
+    id: 'c10',
+    title: 'Summer',
+    color: Colors.teal,
+    description:
+        'Mango, pineapple, dragon fruit, papayas, guavas, star fruit and kiwi–It\'s a seven-way tie. Nothing says summer like one or more of these fruits chopped up and put in a fruit salad, on a fruit tray, or even in a freshly blended smoothie!',
+  ),
+];
