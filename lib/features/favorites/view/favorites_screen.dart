39377c57ba90c6580b14dc49812812d2a5f273dd
RimidalU <stankevichuuladimiru@gmail.com>
2023-09-23 17:02:35 +0300

    feat: implement display favorites

--- lib/features/favorites/view/favorites_screen.dart
+++ lib/features/favorites/view/favorites_screen.dart
@@ -1,5 +1,7 @@
-import 'package:deli_meals_list/features/meals/models/models.dart';
 import 'package:flutter/material.dart';
+
+import '/features/meals/models/models.dart';
+import '/features/meals/widgets/meal_item.dart';
 
 class FavoritesScreen extends StatelessWidget {
   const FavoritesScreen({
@@ -13,9 +15,28 @@ class FavoritesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-          'You have no favorites yet - start adding some!${favoriteMeals.length}'),
-    );
+    if (favoriteMeals.isEmpty) {
+      return Center(
+        child: Text(
+            'You have no favorites yet - start adding some!${favoriteMeals.length}'),
+      );
+    } else {
+      return Container(
+        padding: const EdgeInsets.all(15),
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            return MealItem(
+              affordability: favoriteMeals[index].affordability,
+              complexity: favoriteMeals[index].complexity,
+              duration: favoriteMeals[index].duration,
+              imageUrl: favoriteMeals[index].imageUrl,
+              title: favoriteMeals[index].title,
+              id: favoriteMeals[index].id,
+            );
+          },
+          itemCount: favoriteMeals.length,
+        ),
+      );
+    }
   }
 }
